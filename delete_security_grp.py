c250dec969fd0fdd5d74ff3490acfcadfdb95901
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-08-28 01:22:00 +0530

    Update in code

--- delete_security_grp.py
+++ delete_security_grp.py
@@ -1,33 +1,35 @@
+# by MuZakkir Saifi
+
 # import logging for get the logs in  execution
 import logging
 # import the boto3 which will use to interact  with the aws
 import boto3
 from botocore.exceptions import ClientError
 
-AWS_REGION = input("Please enter the AWS_REGION")
+REGION = input("Please enter the AWS REGION")
 
 # this is the configration for the logger
 
-logger = logging.getLogger()
+logger_for = logging.getLogger()
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s: %(levelname)s: %(message)s')
 
-vpc_client = boto3.client("ec2", region_name=AWS_REGION)
+subnet_client = boto3.client("ec2", region_name=REGION)
 
 
-def delete_security_group(security_group_id):
+def delete_group(group_id):
 
     try:
-        response = vpc_client.delete_security_group(GroupId=security_group_id)
+        res = subnet_client.delete_security_group(GroupId=group_id)
 
     except ClientError:
-        logger.exception('Security group can not be deleted.')
+        logger_for.exception('Security group can not be deleted.')
         raise
     else:
-        return response
+        return res
 
 
 if __name__ == '__main__':
-    SECURITY_GROUP_ID = input("Please enter the Security group id")
-    security_group = delete_security_group(SECURITY_GROUP_ID)
-    logger.info(f'Wow Your security group {SECURITY_GROUP_ID} is deleted successfully.')
+    GRP_ID = input("Please enter the Security group id")   
+    security_group = delete_group(GRP_ID)
+    logger_for.info(f'Wow Your security group {GRP_ID} is deleted successfully.')
